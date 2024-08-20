9ee70eb199ec74d503379360706ede8d7dd0780a
sariya <dayzwaitik123@yandex.ru>
2024-08-20 14:02:37 +0300

    fix indents

--- courses/Разработка веб-сервисов на Go/code/week1/tree/main.go
+++ courses/Разработка веб-сервисов на Go/code/week1/tree/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"path"
+	"strings"
 )
-
-// var listDirsFiles = make([]string, 0)
 
 const (
 	indentSymbol  = "├───"
@@ -23,14 +23,18 @@ type File struct {
 // dirTree выводит список каталогов в указанной директории.
 // Если значение file равно true, то тогда выведутся и
 // файлы в каталогах.
-// func dirTree(out io.Writer, dirName string, file bool) error {
-// 	dirFileList, err := getFileDirList(dirName, file)
+func dirTree(out io.Writer, dirName string, file bool) error {
+	dirFileList, err := getFileDirList(dirName, file)
 
-// 	if err != nil {
-// 		return err
-// 	}
-// 	return nil
-// }
+	if err != nil {
+		return err
+	}
+
+	for i := 0; i < len(dirFileList); i++ {
+		fmt.Fprintf(out, "*%s%s\n", strings.Repeat("-", dirFileList[i].Indent), dirFileList[i].Name)
+	}
+	return nil
+}
 
 func helper(dir string, withFile bool, currList *[]File, indent *int) error {
 	files, err := os.ReadDir(dir)
@@ -47,7 +51,7 @@ func helper(dir string, withFile bool, currList *[]File, indent *int) error {
 				return err
 			}
 		} else if withFile {
-			*currList = append(*currList, File{Name: file.Name(), Indent: *indent})
+			*currList = append(*currList, File{Name: file.Name(), Indent: *indent + 1})
 		}
 	}
 	*indent--
@@ -67,9 +71,12 @@ func getFileDirList(dir string, withFile bool) ([]File, error) {
 }
 
 func main() {
-	list, err := getFileDirList("testdata", false)
+	l, err := getFileDirList("testdata", true)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(list)
+
+	fmt.Printf("\n%v\n", l)
+
+	dirTree(os.Stdout, "testdata", true)
 }
--- courses/Разработка веб-сервисов на Go/code/week1/tree/main_test.go
+++ courses/Разработка веб-сервисов на Go/code/week1/tree/main_test.go
@@ -5,7 +5,8 @@ import (
 	"testing"
 )
 
-const testFullResult = `├───project
+const testFullResult = `
+├───project
 │	├───file.txt (19b)
 │	└───gopher.png (70372b)
 ├───static
@@ -48,7 +49,8 @@ func TestTreeFull(t *testing.T) {
 	}
 }
 
-const testDirResult = `├───project
+const testDirResult = `
+├───project
 ├───static
 │	├───a_lorem
 │	│	└───ipsum
